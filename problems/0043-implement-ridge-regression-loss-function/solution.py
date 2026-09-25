b534d3607f6353670f81f2c0d7436405c815d467
bluetoznic <203263015+bluetoznic@users.noreply.github.com>
2026-09-25 08:47:38 +0000

    Solve: Implement Ridge Regression Loss Function

---
+++ problems/0043-implement-ridge-regression-loss-function/solution.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:
+	# Your code here
+	y_pred=X @ w
+	mse=np.mean((y_pred-y_true)**2)
+	reg=alpha*np.sum(w**2)
+	return float(reg+mse)
+
